b3a0f4096a4a618f94e9e4b39592cfdd16449d2e
Zakaria MOUCHTATI <z.mouchtati@gmail.com>
2025-06-19 11:04:17 +0100

    correct the collection list

--- components/home/ProductsList.tsx
+++ components/home/ProductsList.tsx
@@ -34,44 +34,49 @@ const ProductsList = ({
   if (error) return <ErrorUi />;
 
   return (
-    <div className="my-24 px-4 md:px-8 lg:px-12">
-      <div className="flex flex-col gap-2 text-center items-center">
-        <p className="text-xl text-gray-800 font-serif capitalize  leading-relaxed">
-          {description}
-        </p>
-        <h2 className="text-4xl  text-main uppercase font-serif font-light ">
-          {title}
-        </h2>
-      </div>
+    <>
       {isLoading && <Loading />}
-      <div className="my-12">
-        <Swiper
-          spaceBetween={20}
-          loop={true}
-          autoplay={{ delay: 2000, disableOnInteraction: false }}
-          modules={[Autoplay]}
-          breakpoints={{
-            320: { slidesPerView: 1 }, // Mobile
-            640: { slidesPerView: 2 }, // Small tablets
-            1024: { slidesPerView: 2 }, // Large tablets
-            1280: { slidesPerView: 3 }, // Desktops
-          }}
-          className="w-full"
-        >
-          {data &&
-            data.products.map((product, index) => (
-              <SwiperSlide key={index}>
-                <div className="">
-                  <ProductCard product={product} />
-                </div>
-              </SwiperSlide>
-            ))}
-        </Swiper>
-      </div>
-      {/* {isLoading && <Loading />}
+      {data && data.products.length > 0 && (
+        <div className="my-24 px-4 md:px-8 lg:px-12">
+          <div className="flex flex-col gap-2 text-center items-center">
+            <p className="text-xl text-gray-800 font-serif capitalize  leading-relaxed">
+              {description}
+            </p>
+            <h2 className="text-4xl  text-main uppercase font-serif font-light ">
+              {title}
+            </h2>
+          </div>
+
+          <div className="my-12">
+            <Swiper
+              spaceBetween={20}
+              loop={true}
+              autoplay={{ delay: 2000, disableOnInteraction: false }}
+              modules={[Autoplay]}
+              breakpoints={{
+                320: { slidesPerView: 1 }, // Mobile
+                640: { slidesPerView: 2 }, // Small tablets
+                1024: { slidesPerView: 2 }, // Large tablets
+                1280: { slidesPerView: 3 }, // Desktops
+              }}
+              className="w-full"
+            >
+              {data &&
+                data.products.map((product, index) => (
+                  <SwiperSlide key={index}>
+                    <div className="">
+                      <ProductCard product={product} />
+                    </div>
+                  </SwiperSlide>
+                ))}
+            </Swiper>
+          </div>
+          {/* {isLoading && <Loading />}
       
       */}
-    </div>
+        </div>
+      )}
+    </>
   );
 };
 
